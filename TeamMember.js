c7b3254b57bda78b5d87b070995a42087e4c3d9a
Nguyễn Viết Đạt <datnv7742@ut.edu.vn>
2025-06-21 14:25:32 +0700

    Update TeamMember.js

--- TeamMember.js
+++ TeamMember.js
@@ -1,10 +1,4 @@
-let memberWin = null;
-function openMember(url) {
-  const name = 'memberWindow'; // tên cố định để tái sử dụng tab
-  if (!memberWin || memberWin.closed) {
-    memberWin = window.open(url, name);
-  } else {
-    memberWin.location.href = url;
-    memberWin.focus();
-  }
+function openMember(path) {
+  const basePath = window.location.origin + "/Portfolio.github.io/";
+  window.open(basePath + path.replace("Portfolio.github.io/", ""), "_blank");
 }
